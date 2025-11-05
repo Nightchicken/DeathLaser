2e929ca9b3feacef9f055c05c7ee02314cf14b3e
Joey-The-Human <bowmanjoel196@gmail.com>
2025-11-05 17:46:38 -0600

    Implement servo patrol function in sketch
    Added servo control functionality to sweep two servos.

--- src/helloworld/sketch_oct20a.ino
+++ src/helloworld/sketch_oct20a.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+Servo servo_bottom;
+Servo servo_top;
+
+void setup() {
+  servo_bottom.attach(9, 500, 2500);
+  servo_top.attach(8, 500, 2500);
 }
 
 void loop() {
-  Serial.println("helloWorld");
-  delay(1000);
+  patrol();
+}
+
+void patrol() {
+  // Sweep bottom servo from 0 to 180
+  for (int bottomPos = 0; bottomPos <= 180; bottomPos+=20) {
+    servo_bottom.write(bottomPos);
+    delay(15);
+
+    // Sweep top servo from 0 to 180
+    for (int topPos = 0; topPos <= 180; topPos++) {
+      servo_top.write(topPos);
+      delay(10);
+    }
+
+  // Sweep bottom servo back from 180 to 0
+  for (int bottomPos = 180; bottomPos >= 0; bottomPos-=20) {
+    servo_bottom.write(bottomPos);
+    delay(15);
+
+    // Repeat top servo sweep
+    for (int topPos = 0; topPos <= 180; topPos++) {
+      servo_top.write(topPos);
+      delay(10);
+    }
+  }
+  }
 }
